e70202646e7bc7fc832cc4120265a30bb660c119
DanielWSx64 <danielws.x64@gmail.com>
2019-07-05 11:55:04 -0300

    allow custom request description

--- lib/helpers/document.ex
+++ lib/helpers/document.ex
@@ -1,15 +1,19 @@
 defmodule Xcribe.Helpers.Document do
   alias Xcribe.{ConnParser, Recorder}
 
-  defmacro document(conn) do
+  defmacro document(conn, opts \\ []) do
     "test " <> test_name = __CALLER__.function |> elem(0) |> to_string
 
-    quote bind_quoted: [conn: conn, test_name: test_name] do
+    quote bind_quoted: [conn: conn, test_name: test_name, opts: opts] do
+      options = Keyword.merge([as: test_name], opts)
+
       conn
-      |> ConnParser.execute(test_name)
+      |> ConnParser.execute(request_description(options))
       |> Recorder.save()
 
       conn
     end
   end
+
+  def request_description(options), do: Keyword.fetch!(options, :as)
 end
--- test/lib/helpers/document_test.exs
+++ test/lib/helpers/document_test.exs
@@ -1,10 +1,9 @@
 defmodule Xcribe.Helpers.DocumentTest do
   use Xcribe.ConnCase, async: true
 
-  require Xcribe.Helpers.Document
-
   alias Xcribe.{ConnParser, Recorder}
-  alias Xcribe.Helpers.Document
+
+  import Xcribe.Helpers.Document
 
   describe "document/1" do
     test "parse conn and save it", %{conn: conn} do
@@ -14,10 +13,26 @@ defmodule Xcribe.Helpers.DocumentTest do
         conn
         |> put_req_header("authorization", "token")
         |> get(users_path(conn, :index))
-        |> Document.document()
+        |> document()
 
       test_name = "document/1 parse conn and save it"
       expected_record = [ConnParser.execute(conn, test_name)]
+
+      assert Recorder.get_all() == expected_record
+    end
+
+    test "parse conn and save it whith custom description", %{conn: conn} do
+      Recorder.start_link()
+
+      request_description = "some description"
+
+      conn =
+        conn
+        |> put_req_header("authorization", "token")
+        |> get(users_path(conn, :index))
+        |> document(as: request_description)
+
+      expected_record = [ConnParser.execute(conn, request_description)]
 
       assert Recorder.get_all() == expected_record
     end
