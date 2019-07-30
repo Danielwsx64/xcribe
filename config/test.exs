f610a670ea49efcf03445b0ed6aabd2a8e17c4cc
DanielWSx64 <danielws.x64@gmail.com>
2019-07-30 12:06:36 -0300

    use configuration to file name

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :xcribe, :information_source, Xcribe.Support.Information
+config :xcribe, :output_file, "teste.apib"
 
 # Support Config
 import_config "test_support.exs"
--- lib/writter.ex
+++ lib/writter.ex
@@ -7,10 +7,12 @@ defmodule Xcribe.Writter do
   This writes the given text to the configured output file
   """
   def write(text) do
-    {:ok, file} = File.open("file.md", [:write])
+    {:ok, file} = File.open(file_name(), [:write])
 
     IO.binwrite(file, text)
 
     File.close(file)
   end
+
+  defp file_name, do: Application.get_env(:xcribe, :output_file, "api_doc.apib")
 end
