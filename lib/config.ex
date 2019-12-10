90c4179767da66949966173c24af1699d6eedda4
brosquinha <thales@brainn.co>
2019-12-10 13:56:10 -0300

    Add Swagger module to Xcribe.Formatter

--- lib/config.ex
+++ lib/config.ex
@@ -1,6 +1,8 @@
 defmodule Xcribe.Config do
   def output_file, do: Application.get_env(:xcribe, :output_file, "api_doc.apib")
 
+  def api_format, do: Application.get_env(:xcribe, :doc_format, :api_blueprint)
+
   def active?, do: !is_nil(System.get_env(env_var_name()))
 
   def xcribe_information_source,
--- lib/formatter.ex
+++ lib/formatter.ex
@@ -1,7 +1,7 @@
 defmodule Xcribe.Formatter do
   use GenServer
 
-  alias Xcribe.{ApiBlueprint, Config, Recorder, Writter}
+  alias Xcribe.{ApiBlueprint, Config, Recorder, Swagger, Writter}
 
   def init(_config) do
     {:ok, nil}
@@ -10,7 +10,7 @@ defmodule Xcribe.Formatter do
   def handle_cast({:suite_finished, _run_us, _load_us}, nil) do
     if Config.active?() do
       Recorder.get_all()
-      |> ApiBlueprint.generate_doc()
+      |> generate_docs(Config.api_format())
       |> Writter.write()
     end
 
@@ -18,4 +18,7 @@ defmodule Xcribe.Formatter do
   end
 
   def handle_cast(_event, nil), do: {:noreply, nil}
+
+  defp generate_docs(requests, :swagger), do: Swagger.generate_doc(requests)
+  defp generate_docs(requests, _), do: ApiBlueprint.generate_doc(requests)
 end
