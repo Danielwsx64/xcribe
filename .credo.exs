5359b97a29ba3b99fe38bae929253027d529c6fd
DanielWSx64 <danielws.x64@gmail.com>
2020-04-01 11:59:12 -0300

    Remove useless config from credo.exs

--- .credo.exs
+++ .credo.exs
@@ -21,8 +21,8 @@
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
         #
-        included: ["lib/", "src/", "test/", "web/", "apps/"],
-        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/", ~r"/test/support/"]
+        included: ["lib/", "test/"],
+        excluded: [~r"/test/support/"]
       },
       #
       # If you create your own checks, you must specify the source files for
