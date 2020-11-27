88080597990ac8433e02c75f0f0856543a58c1da
DanielWSx64 <danielws.x64@gmail.com>
2020-11-27 09:35:36 -0300

    Add request validator

---
+++ lib/xcribe/request/validator.ex
@@ -0,0 +1,52 @@
+defmodule Xcribe.Request.Validator do
+  alias Xcribe.Request
+  alias Xcribe.Request.Error
+
+  def validate(%Request{} = request) do
+    {:ok, request}
+    |> validate_parameters_in(:request_body)
+    |> validate_parameters_in(:path_params)
+    |> validate_parameters_in(:header_params)
+    |> validate_parameters_in(:query_params)
+    |> handle_validate(request)
+  end
+
+  defp validate_parameters_in({:error, _err} = error, _key), do: error
+
+  defp validate_parameters_in({:ok, request}, key) do
+    request
+    |> Map.fetch!(key)
+    |> find_struct()
+    |> handle_validate_params(request)
+  end
+
+  def find_struct(%{__struct__: module}) do
+    %Error{
+      type: :validation,
+      message: "The Plug.Conn params must be valid HTTP params. A struct #{module} was found!"
+    }
+  end
+
+  def find_struct(%{} = map), do: Enum.reduce_while(map, :ok, &reduce_map/2)
+  def find_struct(list) when is_list(list), do: Enum.reduce_while(list, :ok, &reduce_list/2)
+  def find_struct(_), do: :ok
+
+  defp reduce_list(value, _acc), do: reduce_function(value)
+
+  defp reduce_map({_key, value}, _acc), do: reduce_function(value)
+
+  defp reduce_function(value) do
+    case find_struct(value) do
+      :ok -> {:cont, :ok}
+      %Error{} = error -> {:halt, error}
+    end
+  end
+
+  defp handle_validate_params(:ok, request), do: {:ok, request}
+  defp handle_validate_params(%Error{} = error, _request), do: {:error, error}
+
+  defp handle_validate({:ok, _req} = success, _request), do: success
+
+  defp handle_validate({:error, error}, request),
+    do: {:error, %{error | __meta__: request.__meta__}}
+end
---
+++ test/xcribe/request/validator_test.exs
@@ -0,0 +1,126 @@
+defmodule Xcribe.Request.ValidatorTest do
+  use ExUnit.Case, async: true
+
+  alias Xcribe.Request
+  alias Xcribe.Request.{Error, Validator}
+
+  describe "validate/1" do
+    test "return ok when request is valid" do
+      request = %Request{
+        action: "index",
+        controller: Elixir.Xcribe.UsersController,
+        description: "",
+        header_params: [{"authorization", "token"}],
+        params: %{},
+        path: "/users",
+        path_params: %{},
+        query_params: %{},
+        request_body: %{},
+        resource: "users",
+        resource_group: :api,
+        resp_body: "[{\"id\":1,\"name\":\"user 1\"},{\"id\":2,\"name\":\"user 2\"}]",
+        resp_headers: [
+          {"content-type", "application/json; charset=utf-8"},
+          {"cache-control", "max-age=0, private, must-revalidate"}
+        ],
+        status_code: 200,
+        verb: "get"
+      }
+
+      assert Validator.validate(request) == {:ok, request}
+    end
+
+    test "return an error when request has a struct in path_params" do
+      meta = %{metadata: "value"}
+      request = %Request{path_params: ~D[2021-02-15], __meta__: meta}
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Error{
+                  type: :validation,
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  __meta__: meta
+                }}
+    end
+
+    test "return an error when request has a struct in request_body" do
+      meta = %{metadata: "value"}
+      request = %Request{request_body: ~D[2021-02-15], __meta__: meta}
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Error{
+                  type: :validation,
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  __meta__: meta
+                }}
+    end
+
+    test "return an error when request has a struct in header_params" do
+      meta = %{metadata: "value"}
+      request = %Request{header_params: ~D[2021-02-15], __meta__: meta}
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Error{
+                  type: :validation,
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  __meta__: meta
+                }}
+    end
+
+    test "return an error when request has a struct in query_params" do
+      meta = %{metadata: "value"}
+      request = %Request{query_params: ~D[2021-02-15], __meta__: meta}
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Error{
+                  type: :validation,
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  __meta__: meta
+                }}
+    end
+
+    test "params with nested struct" do
+      request = %Request{
+        request_body: %{
+          date: ~D[2021-02-15]
+        }
+      }
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Xcribe.Request.Error{
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  type: :validation
+                }}
+    end
+
+    test "params with nested struct into list" do
+      request = %Request{
+        request_body: %{
+          params: [
+            1,
+            %{
+              date: ~D[2021-02-15]
+            }
+          ]
+        }
+      }
+
+      assert Validator.validate(request) ==
+               {:error,
+                %Xcribe.Request.Error{
+                  message:
+                    "The Plug.Conn params must be valid HTTP params. A struct Elixir.Date was found!",
+                  type: :validation
+                }}
+    end
+  end
+end
