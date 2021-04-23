5cb57696c9893755ac6996102d68f47019378e64
Thales César <thalesgiriboni@gmail.com>
2021-04-23 08:59:08 -0300

    Fix PR issues

--- test/xcribe/writter_test.exs
+++ test/xcribe/writter_test.exs
@@ -5,20 +5,29 @@ defmodule Xcribe.WritterTest do
 
   alias Xcribe.Writter
 
-  @output_path "/tmp/test"
-  @invalid_output_path "/root/null"
+  setup do
+    on_exit(fn ->
+      Application.delete_env(:xcribe, :output)
+    end)
+
+    :ok
+  end
 
   describe "write/1" do
     test "writes given text to set output directory" do
-      Application.put_env(:xcribe, :output, @output_path)
+      output_path = "/tmp/xcribe_#{:rand.uniform()}"
+      Application.put_env(:xcribe, :output, output_path)
 
       assert capture_io(fn ->
                assert :ok == Writter.write("sample test")
-             end) =~ "Xcribe documentation written in #{@output_path}"
+             end) =~ "Xcribe documentation written in #{output_path}"
+
+      assert File.read!(output_path) == "sample test"
     end
 
     test "raises InvalidOutputDestination if output directory cannot be accessed" do
-      Application.put_env(:xcribe, :output, @invalid_output_path)
+      invalid_output_path = "/root/null"
+      Application.put_env(:xcribe, :output, invalid_output_path)
 
       assert capture_io(fn ->
                assert :error == Writter.write("sample test")
