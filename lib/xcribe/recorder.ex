2f2b3a19cab403e4d4ff4c99aa3343a8a164c99a
Danielwsx64 <danielws.x64@gmail.com>
2021-06-24 15:36:01 -0300

    Add a clear function to Recorder

--- lib/xcribe/recorder.ex
+++ lib/xcribe/recorder.ex
@@ -5,31 +5,19 @@ defmodule Xcribe.Recorder do
 
   alias Xcribe.{Request, Request.Error}
 
-  def start_link(_opts \\ []) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
-  end
+  def start_link(_opts \\ []), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
-  def init(state) do
-    {:ok, state}
-  end
+  def init(state), do: {:ok, state}
 
-  def save(%Request{} = request) do
-    GenServer.cast(__MODULE__, {:save, request})
-  end
+  def save(%Request{} = request), do: GenServer.cast(__MODULE__, {:save, request})
+  def save(%Error{} = error), do: GenServer.cast(__MODULE__, {:save, error})
 
-  def save(%Error{} = error) do
-    GenServer.cast(__MODULE__, {:save, error})
-  end
+  def get_all, do: GenServer.call(__MODULE__, :get_all)
 
-  def get_all do
-    GenServer.call(__MODULE__, :get_all)
-  end
+  def clear, do: GenServer.call(__MODULE__, :clear)
 
-  def handle_cast({:save, request}, records) do
-    {:noreply, [request | records]}
-  end
+  def handle_cast({:save, request}, records), do: {:noreply, [request | records]}
 
-  def handle_call(:get_all, _from, records) do
-    {:reply, records, records}
-  end
+  def handle_call(:get_all, _from, records), do: {:reply, records, records}
+  def handle_call(:clear, _from, records), do: {:reply, records, []}
 end
--- test/xcribe/recorder_test.exs
+++ test/xcribe/recorder_test.exs
@@ -1,12 +1,16 @@
 defmodule Xcribe.RecorderTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias Xcribe.{Recorder, Request, Request.Error}
 
+  setup do
+    on_exit(fn ->
+      Recorder.clear()
+    end)
+  end
+
   describe "save and get requests records" do
     test "use genserver to save and recover requests" do
-      Recorder.start_link()
-
       request_one = %Request{description: "first request"}
       request_two = %Request{description: "second request"}
       request_error = %Error{message: "some error"}
@@ -16,6 +20,21 @@ defmodule Xcribe.RecorderTest do
       Recorder.save(request_error)
 
       assert Recorder.get_all() == [request_error, request_two, request_one]
+    end
+  end
+
+  describe "clear saved records" do
+    test "use genserver to save and recover requests" do
+      request_one = %Request{description: "first request"}
+      request_two = %Request{description: "second request"}
+      request_error = %Error{message: "some error"}
+
+      Recorder.save(request_one)
+      Recorder.save(request_two)
+      Recorder.save(request_error)
+
+      assert Recorder.clear() == [request_error, request_two, request_one]
+      assert Recorder.get_all() == []
     end
   end
 end
